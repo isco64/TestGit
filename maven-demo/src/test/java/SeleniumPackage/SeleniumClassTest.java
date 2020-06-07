a92c142dba286dc7f302d27937b3057de65b41a6
Isco <Isco@DESKTOP-P4JN8D2>
2020-06-06 21:22:25 -0400

    commit 6

--- maven-demo/src/test/java/SeleniumPackage/SeleniumClassTest.java
+++ maven-demo/src/test/java/SeleniumPackage/SeleniumClassTest.java
@@ -4,16 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumClassTest {
 	
 	@Test
 	public void SeleniumTestFunction() 
 	{
-		
 		System.out.println("Andate a la xuxa");
-		WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Isco\\Desktop\\Esudios\\Quinto Semestre\\Tecnicas de Calidad\\Selenium\\chromedriver.exe" );
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://google.cl");
 	}
