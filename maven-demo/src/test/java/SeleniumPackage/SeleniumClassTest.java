c870b9cdc32372502e9780c38dd67036ff49f863
Isco <Isco@DESKTOP-P4JN8D2>
2020-06-08 21:06:25 -0400

    commit 9

--- maven-demo/src/test/java/SeleniumPackage/SeleniumClassTest.java
+++ maven-demo/src/test/java/SeleniumPackage/SeleniumClassTest.java
@@ -12,17 +12,18 @@ public class SeleniumClassTest {
 	@Test
 	public void SeleniumTestFunction() 
 	{
-		System.out.println("Cargando Testing");
+		System.out.println("Cargabdi");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Isco\\Desktop\\Esudios\\Quinto Semestre\\Tecnicas de Calidad\\Selenium\\chromedriver.exe" );
 		WebDriver driver = new ChromeDriver();
 		
-        String baseUrl = "http://localhost:8080/CarritoComprasWeb/";
+        //String baseUrl = "http://localhost:8080/CarritoComprasWeb/";
+        String baseUrl = "http://www.duoc.cl/inicio";
         
         driver.get(baseUrl);
         driver.manage().window().maximize();
         
         // click en link
-        driver.findElement(By.linkText("Carrito de Compras")).click();
+        //driver.findElement(By.linkText("Carrito de Compras")).click();
 	}
 
 }
